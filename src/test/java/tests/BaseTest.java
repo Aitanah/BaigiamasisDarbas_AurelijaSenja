9d1a5e98f460743d60865c3e021118609c93d2a9
Aurelija <aurelija.zemkute@gmail.com>
2022-05-26 13:41:36 +0300

    Add annotations: Listeners, BeforeMethod, AfterMethod

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,4 +1,22 @@
 package tests;
 
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
+import utilities.Driver;
+import utilities.TestListener;
+
+@Listeners(TestListener.class)
+
 public class BaseTest {
+
+    @BeforeMethod
+    public void setup() {
+        Driver.setDriver();
+    }
+
+    @AfterMethod
+    public void teardown() {
+        Driver.closeDriver();
+    }
 }
