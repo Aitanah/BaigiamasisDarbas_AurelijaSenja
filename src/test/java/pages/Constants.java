796822bc496fef0333b21f17b6138f509f15adaf
Aurelija <aurelija.zemkute@gmail.com>
2022-05-26 21:49:07 +0300

    Update duration time

--- src/test/java/pages/Constants.java
+++ src/test/java/pages/Constants.java
@@ -4,7 +4,7 @@ import java.time.Duration;
 
 public class Constants {
 
-    public static Duration WAIT = Duration.ofSeconds(5);
+    public static Duration WAIT = Duration.ofSeconds(10);
     public static String EMAIL = "testdata1@gmail.com";
     public static String PASSWORD = "TestData123";
 }
