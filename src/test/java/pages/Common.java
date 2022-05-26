c5d6e9b83720246a1259341ae827b1a9e3ba1105
Aurelija <aurelija.zemkute@gmail.com>
2022-05-27 02:41:33 +0300

    Add methods for Test - createCollection

--- src/test/java/pages/Common.java
+++ src/test/java/pages/Common.java
@@ -2,7 +2,11 @@ package pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import utilities.Driver;
+
+import java.util.Random;
 
 public class Common {
 
@@ -10,7 +14,7 @@ public class Common {
         Driver.getDriver().get(url);
     }
 
-    public static WebElement getElement(By locator) {
+    private static WebElement getElement(By locator) {
         return Driver.getDriver().findElement(locator);
     }
 
@@ -26,4 +30,33 @@ public class Common {
         getElement(locator).sendKeys(keys);
     }
 
+    public static String getElementText(By locator) {
+        return getElement(locator).getText();
+    }
+
+    public static void waitForElementToBeClickable(By locator) {
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), Constants.WAIT_LONG);
+        wait.until(ExpectedConditions.elementToBeClickable(locator));
+    }
+
+    public static void waitForElementToBeVisible(By locator) {
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), Constants.WAIT_LONG);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
+
+    public static void waitForElementToBeInvisible(By locator) {
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), Constants.WAIT_LONG);
+        wait.until(ExpectedConditions.invisibilityOfElementLocated(locator));
+    }
+
+    public static String generateRandomString(String characters, int length) {
+        Random r = new Random();
+        char[] text = new char[length];
+        for (int i = 0; i < length; i++) {
+            text[i] = characters.charAt(r.nextInt(characters.length()));
+        }
+        return new String(text);
+    }
 }
+
+
--- src/test/java/pages/etsy/AccountProfile.java
+++ src/test/java/pages/etsy/AccountProfile.java
@@ -20,4 +20,41 @@ public class AccountProfile {
         Common.sendKeysToElement(Locators.Etsy.AccountProfile.inputPassword, Constants.PASSWORD);
         Common.clickElement(Locators.Etsy.AccountProfile.buttonSignInForm);
     }
+
+    public static String generateRandomCollectionName() {
+        return Common.generateRandomString(Constants.SOURCES, 10);
+    }
+    public static void clickAccountMenuButton() {
+        Common.waitForElementToBeClickable(Locators.Etsy.AccountProfile.frameAccountMenuButton);
+        Common.clickElement(Locators.Etsy.AccountProfile.buttonAccountMenu);
+    }
+
+    public static void clickViewYourAccountButton() {
+        Common.waitForElementToBeClickable(Locators.Etsy.AccountProfile.frameAccountMenu);
+        Common.clickElement(Locators.Etsy.AccountProfile.buttonViewYourAccount);
+    }
+
+    public static void closePopUpWindow() {
+        Common.waitForElementToBeClickable(Locators.Etsy.AccountProfile.framePopUpWindow);
+        Common.clickElement(Locators.Etsy.AccountProfile.buttonClosePopUpWindow);
+    }
+
+    public static void clickCreateCollectionButton() {
+        Common.clickElement(Locators.Etsy.AccountProfile.buttonCreateCollection);
+    }
+
+    public static void enterCollectionName(String collectionName) {
+        Common.sendKeysToElement(Locators.Etsy.AccountProfile.inputCollectionName, collectionName);
+    }
+
+    public static void clickCreateCollectionButtonOnCreateCollectionWindow() {
+        Common.clickElement(Locators.Etsy.AccountProfile.buttonCreateCollectionOnPopUpWindow);
+    }
+
+    public static String readCollectionName() {
+        Common.waitForElementToBeInvisible(Locators.Etsy.AccountProfile.fieldCreatCollectionWindow);
+        return Common.getElementText(Locators.Etsy.AccountProfile.fieldCollections);
+    }
+
+
 }
