4b655c1ecddd8e3a6da54796336117b7d4ebef43
Aurelija <aurelija.zemkute@gmail.com>
2022-05-27 02:38:37 +0300

    Add more constants

--- src/test/java/pages/Constants.java
+++ src/test/java/pages/Constants.java
@@ -4,7 +4,9 @@ import java.time.Duration;
 
 public class Constants {
 
-    public static Duration WAIT = Duration.ofSeconds(10);
+    public static Duration WAIT_SHORT = Duration.ofSeconds(3);
+    public static Duration WAIT_LONG = Duration.ofSeconds(10);
     public static String EMAIL = "testdata1@gmail.com";
     public static String PASSWORD = "TestData123";
+    public static String SOURCES = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
 }
