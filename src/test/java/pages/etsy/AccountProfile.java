ba82d0ac5c21d5f692b438dd1806df7e336ec142
Aurelija <aurelija.zemkute@gmail.com>
2022-05-26 21:52:44 +0300

    Create commands and steps for BeforeMethod at AccountProfile class

--- src/test/java/pages/etsy/AccountProfile.java
+++ src/test/java/pages/etsy/AccountProfile.java
@@ -1,4 +1,23 @@
 package pages.etsy;
 
+import pages.Common;
+import pages.Constants;
+import pages.Locators;
+
 public class AccountProfile {
+
+    public static void open() {
+        Common.openUrl("https://www.etsy.com");
+    }
+
+    public static void clickToAcceptCookies() {
+        Common.acceptCookies(Locators.Etsy.AccountProfile.buttonAcceptCookies);
+    }
+
+    public static void signIn() {
+        Common.clickElement(Locators.Etsy.AccountProfile.buttonSignInHomePage);
+        Common.sendKeysToElement(Locators.Etsy.AccountProfile.inputEmail, Constants.EMAIL);
+        Common.sendKeysToElement(Locators.Etsy.AccountProfile.inputPassword, Constants.PASSWORD);
+        Common.clickElement(Locators.Etsy.AccountProfile.buttonSignInForm);
+    }
 }
--- src/test/java/tests/etsy/AccountProfile.java
+++ src/test/java/tests/etsy/AccountProfile.java
@@ -1,4 +1,20 @@
 package tests.etsy;
 
-public class AccountProfile {
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import tests.BaseTest;
+
+public class AccountProfile extends BaseTest {
+
+    @BeforeMethod
+    public void openPage() {
+        pages.etsy.AccountProfile.open();
+        pages.etsy.AccountProfile.clickToAcceptCookies();
+        pages.etsy.AccountProfile.signIn();
+    }
+
+    @Test
+    public void createCollection() {
+
+    }
 }
