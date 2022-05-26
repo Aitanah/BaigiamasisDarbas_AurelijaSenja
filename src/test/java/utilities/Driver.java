24e7f06781a319ba05c237386f69d0300fbfd3fb
Aurelija <aurelija.zemkute@gmail.com>
2022-05-26 16:05:07 +0300

    Add Chrome options

--- src/test/java/utilities/Driver.java
+++ src/test/java/utilities/Driver.java
@@ -3,6 +3,7 @@ package utilities;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class Driver {
 
@@ -10,6 +11,9 @@ public class Driver {
 
     public static void setDriver() {
         WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+//        options.addArguments("headless");
+        options.addArguments("start-maximized");
         driver.set(new ChromeDriver());
     }
 
