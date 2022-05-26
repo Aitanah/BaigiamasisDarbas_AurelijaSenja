48035a3d471926401bc0c83b5ae4dd5e7b8dfc1b
Aurelija <aurelija.zemkute@gmail.com>
2022-05-26 21:52:08 +0300

    Create common commands for BeforeMethod at AccountProfile class

--- src/test/java/pages/Common.java
+++ src/test/java/pages/Common.java
@@ -1,4 +1,29 @@
 package pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import utilities.Driver;
+
 public class Common {
+
+    public static void openUrl(String url) {
+        Driver.getDriver().get(url);
+    }
+
+    public static WebElement getElement(By locator) {
+        return Driver.getDriver().findElement(locator);
+    }
+
+    public static void clickElement(By locator) {
+        getElement(locator).click();
+    }
+
+    public static void acceptCookies(By locator) {
+        clickElement(locator);
+    }
+
+    public static void sendKeysToElement(By locator, String keys) {
+        getElement(locator).sendKeys(keys);
+    }
+
 }
