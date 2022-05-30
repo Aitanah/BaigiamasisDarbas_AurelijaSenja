1bd0327214914120176baaf9ed8dcc21ec529d99
Aurelija <aurelija.zemkute@gmail.com>
2022-05-30 03:35:50 +0300

    Create test: sortByLowestPrice

--- src/test/java/pages/etsy/Vintage.java
+++ src/test/java/pages/etsy/Vintage.java
@@ -1,4 +1,38 @@
 package pages.etsy;
 
-public class Vintage {
+import pages.Common;
+import pages.Locators;
+import tests.BaseTest;
+import java.util.Collections;
+import java.util.List;
+
+public class Vintage extends BaseTest {
+
+    public static void open() {
+        Common.openUrl("https://www.etsy.com/c/vintage?ref=vintage_nav_promo");
+    }
+
+    public static void clickToAcceptCookies() {
+        Common.acceptCookies(Locators.Etsy.buttonAcceptCookies);
+    }
+
+    public static void clickSortByButton() {
+        Common.clickElement(Locators.Etsy.Vintage.buttonSortBy);
+    }
+
+    public static void selectLowestPriceButton() {
+        Common.clickElement(Locators.Etsy.Vintage.buttonLowestPrice);
+    }
+
+    public static List<String> readProductsPrices() {
+        List<String> productPricesList = Common.getElementTextListByLocator(Locators.Etsy.Vintage.fieldPrices);
+        return productPricesList;
+    }
+
+    public static List<String> sortProductPrices() {
+        List<String> list = Common.getElementTextListByLocator(Locators.Etsy.Vintage.fieldPrices);
+        Collections.sort(list);
+        return list;
+    }
+
 }
--- src/test/java/tests/etsy/Vintage.java
+++ src/test/java/tests/etsy/Vintage.java
@@ -1,4 +1,27 @@
 package tests.etsy;
 
-public class Vintage {
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import tests.BaseTest;
+
+public class Vintage extends BaseTest {
+
+    @BeforeMethod
+    public void openPage() {
+        pages.etsy.Vintage.open();
+        pages.etsy.Vintage.clickToAcceptCookies();
+    }
+
+    @Test
+    public void sortByLowestPrice() {
+        String expectedPrices = String.valueOf(pages.etsy.Vintage.sortProductPrices());
+
+        pages.etsy.Vintage.clickSortByButton();
+        pages.etsy.Vintage.selectLowestPriceButton();
+
+        String actualPrices = String.valueOf(pages.etsy.Vintage.readProductsPrices());
+
+        Assert.assertEquals(actualPrices, expectedPrices);
+    }
 }
