8aacb467a3bf7034da27823601e96a7d62ce1b59
Aurelija <aurelija.zemkute@gmail.com>
2022-05-30 03:36:22 +0300

    Create test: searchForAnything

--- src/test/java/pages/etsy/Home.java
+++ src/test/java/pages/etsy/Home.java
@@ -1,4 +1,59 @@
 package pages.etsy;
 
+import pages.Common;
+import pages.Constants;
+import pages.Locators;
+
 public class Home {
+
+//    For BeforeMethod at AccountProfile
+    public static void open() {
+        Common.openUrl("https://www.etsy.com");
+    }
+
+    public static void clickToAcceptCookies() {
+        Common.acceptCookies(Locators.Etsy.buttonAcceptCookies);
+    }
+
+    public static void signIn() {
+        Common.clickElement(Locators.Etsy.Home.buttonSignInHomePage);
+        Common.sendKeysToElement(Locators.Etsy.Home.inputEmail, Constants.EMAIL);
+        Common.sendKeysToElement(Locators.Etsy.Home.inputPassword, Constants.PASSWORD);
+        Common.clickElement(Locators.Etsy.Home.buttonSignInForm);
+    }
+
+//    For Test: createCollection at AccountProfile
+    public static void clickAccountMenuButton() {
+        Common.waitForElementToBeClickable(Locators.Etsy.Home.buttonAccountMenu);
+        Common.clickElement(Locators.Etsy.Home.buttonAccountMenu);
+    }
+
+    public static void selectViewYourAccountButton() {
+        Common.waitForElementToBeClickable(Locators.Etsy.Home.buttonViewYourAccount);
+        Common.clickElement(Locators.Etsy.Home.buttonViewYourAccount);
+    }
+
+//    For Test: updateAccountPreferences at AccountProfile
+    public static void selectAccountSettingsButton() {
+        Common.waitForElementToBeClickable(Locators.Etsy.Home.buttonAccountSettings);
+        Common.clickElement(Locators.Etsy.Home.buttonAccountSettings);
+    }
+
+//    For Test:searchForAnything at Home
+    public static void enterSearchObjectName(String searchObjectName) {
+        Common.sendKeysToElement(Locators.Etsy.Home.inputSearchBox, searchObjectName);
+    }
+
+    public static void clickSearchButton() {
+        Common.clickElement(Locators.Etsy.Home.buttonSearch);
+    }
+
+    public static String readSearchText() {
+        return Common.getElementText(Locators.Etsy.Home.fieldSearchResults);
+    }
+
+//    For Test:addToCart at AddToCart
+    public static void clickOnProduct() {
+        Common.clickElement(Locators.Etsy.Home.buttonProduct);
+    }
 }
--- src/test/java/tests/etsy/Home.java
+++ src/test/java/tests/etsy/Home.java
@@ -1,4 +1,27 @@
 package tests.etsy;
 
-public class Home {
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import tests.BaseTest;
+
+public class Home extends BaseTest {
+
+    @BeforeMethod
+    public void openPage() {
+        pages.etsy.Home.open();
+        pages.etsy.Home.clickToAcceptCookies();
+    }
+
+    @Test
+    public void searchForAnything() {
+        String expectedSearchText = "Golden Ring";
+
+        pages.etsy.Home.enterSearchObjectName(expectedSearchText);
+        pages.etsy.Home.clickSearchButton();
+
+        String actualSearchText = pages.etsy.Home.readSearchText();
+
+        Assert.assertTrue(actualSearchText.contains(expectedSearchText));
+    }
 }
